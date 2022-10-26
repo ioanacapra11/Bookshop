2ea27ad66e2be3ff9d16f3106c519d9a9bc6b1a1
ioanacapra11 <102293162+ioanacapra11@users.noreply.github.com>
2022-10-26 23:54:40 +0300

    added validations for signin and signup methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,15 +8,21 @@ const firebaseApp = require("../database.js");
 
 exports.signup = async (req, res) => {
   const auth = getAuth();
-  createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
+  await createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
     .then((userCredential) => {
       const user = userCredential.user;
-      return res.json(user);
+      return res.status(200).json(user);
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      return res.json({
+      if (errorCode == "auth/email-already-in-use") {
+        return res.status(409).send("user with email already exists");
+      }
+      if (errorCode == "auth/weak-password") {
+        return res.status(400).send("the password is too weak");
+      }
+      return res.status(401).json({
         errorCode,
         errorMessage,
       });
@@ -24,14 +30,25 @@ exports.signup = async (req, res) => {
 };
 
 exports.signin = async (req, res) => {
-  try {
-    const userSignIn = await signInWithEmailAndPassword(
-      getAuth(firebaseApp),
-      req.body.email,
-      req.body.password
-    );
-    return res.json(userSignIn);
-  } catch (err) {
-    res.status(401).json({ error: err.message });
-  }
+  const auth = getAuth(firebaseApp);
+  await signInWithEmailAndPassword(auth, req.body.email, req.body.password)
+    .then(() => {
+      return res.status(200).send("user signed in successfully");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode == "auth/wrong-password") {
+        return res.status(403).send("wrong password");
+      }
+      if (errorCode == "auth/user-not-found") {
+        return res
+          .status(403)
+          .send(`user with email ${req.body.email} does not exist`);
+      }
+      return res.status(401).json({
+        errorCode,
+        errorMessage,
+      });
+    });
 };
